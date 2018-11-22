a23b32fd2800927add232953422567fffd84ad57
Jordan <jche681@aucklanduni.ac.nz>
2018-11-23 00:40:12 +1300

    Basic layout with api calls

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,71 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Grid } from '@material-ui/core';
 
-class App extends React.Component {
+interface IState {
+  currentMovie: any,
+  movies: any[],
+  loading: boolean,
+}
+
+class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      currentMovie: null,
+      movies: [],
+      loading: true
+    }
+
+    this.fetchSavedMovies()
+    this.selectNewMovie = this.selectNewMovie.bind(this)
+
+  }
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movies App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
+        <div>
+          <Grid container={true} justify="center" alignItems="center" spacing={24}>
+            <div/>
+          </Grid>
+        </div>
+        
+        {this.state.loading ?  <CircularProgress /> : 
+          <div>test</div>}
       </div>
     );
+  }
+
+  // GET movies
+  private fetchSavedMovies() {
+    let url = "http://msa2018moviesapi.azurewebsites.net/api/movie"
+    fetch(url, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(json => {
+        let currentMovie = json[0]
+        this.setState({
+          currentMovie,
+          movies: json,
+          loading: false
+        })
+      })
+      .then(json => console.log(json));
+  }
+
+	// Change selected movie
+	private selectNewMovie(newMovie: any) {
+		this.setState({
+			currentMovie: newMovie
+		})
   }
 }
 
